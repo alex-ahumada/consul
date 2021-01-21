815472d19d5e4afec80ea6677541e27aacab75a8
Javi Martín <javim@elretirao.net>
2021-01-21 19:06:01 +0100

    Add list of targets to SDG tag list

--- app/components/sdg/goals/tag_list_component.rb
+++ app/components/sdg/goals/tag_list_component.rb
@@ -13,12 +13,16 @@ class SDG::Goals::TagListComponent < ApplicationComponent
 
   private
 
-    def links
-      [*goal_links, see_more_link]
+    def record
+      record_or_name if record_or_name.respond_to?(:sdg_goals)
+    end
+
+    def goal_links_with_see_more
+      [*goal_links, see_more_link("goals")]
     end
 
     def goal_links
-      goals.limit(limit).map do |goal|
+      goals.order(:code).limit(limit).map do |goal|
         [
           render(SDG::Goals::IconComponent.new(goal)),
           index_by_goal(goal),
@@ -28,31 +32,54 @@ class SDG::Goals::TagListComponent < ApplicationComponent
     end
 
     def goals
-      if record_or_name.respond_to?(:sdg_goals)
-        record_or_name.sdg_goals.order(:code)
-      else
-        SDG::Goal.order(:code)
+      record&.sdg_goals || SDG::Goal.all
+    end
+
+    def target_links_with_see_more
+      [*target_links, see_more_link("targets")]
+    end
+
+    def target_links
+      targets.sort[0..(limit.to_i - 1)].map do |target|
+        [
+          "#{SDG::Target.model_name.human} #{target.code}",
+          index_by_target(target),
+          title: filter_text(target),
+          data: { code: target.code }
+        ]
       end
     end
 
-    def see_more_link
+    def targets
+      record&.sdg_targets || []
+    end
+
+    def see_more_link(namespace)
       return unless limit && count_out_of_limit > 0
 
       [
         "#{count_out_of_limit}+",
         polymorphic_path(record_or_name),
-        class: "more-goals", title: t("sdg.goals.filter.more", count: count_out_of_limit)
+        class: "more-goals", title: t("sdg.#{namespace}.filter.more", count: count_out_of_limit)
       ]
     end
 
     def index_by_goal(goal)
-      polymorphic_path(model, advanced_search: { goal: goal.code })
+      index_by(goal: goal.code)
     end
 
-    def filter_text(goal)
-      t("sdg.goals.filter.link",
+    def index_by_target(target)
+      index_by(target: target.code)
+    end
+
+    def index_by(advanced_search)
+      polymorphic_path(model, advanced_search: advanced_search)
+    end
+
+    def filter_text(goal_or_target)
+      t("sdg.#{goal_or_target.model_name.element.pluralize}.filter.link",
         resources: model.model_name.human(count: :other),
-        code: goal.code)
+        code: goal_or_target.code)
     end
 
     def count_out_of_limit
